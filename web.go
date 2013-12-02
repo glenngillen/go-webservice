f699ee57e7f98f26e919cb0b7db77b7c3c51373f
Glenn Gillen <me@glenngillen.com>
2013-12-02 09:07:03 -0800

    Playing with go for webservices.

---
+++ web.go
@@ -0,0 +1,37 @@
+package main
+
+// Import formatting and IO libraries
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// Define a structure to hold our page
+type Page struct {
+	Title string
+	Body  []byte // IO libs expect a byte slice rather than a string
+}
+
+// Add a save method to our Page struct so we can persist our data
+// This method's signature reads: "This is a method named save that takes as its receiver p, a pointer to Page . It takes no parameters, and returns a value of type error."
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+// Load pages too
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
