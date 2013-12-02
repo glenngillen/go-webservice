a97e51d7adef8c07e5a59306bea6a35b48b2eeed
Glenn Gillen <me@glenngillen.com>
2013-12-02 09:24:16 -0800

    Redirect to edit if page doesn't exist.

--- web.go
+++ web.go
@@ -32,14 +32,18 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    t, _ := template.ParseFiles(tmpl + ".html")
-        t.Execute(w, p)
-        }
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-        renderTemplate(w, "view", p)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,7 +52,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-        renderTemplate(w, "edit", p)
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
